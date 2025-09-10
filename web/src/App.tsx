9e73f57977efcd99779be94723133cd1809f0f41
Jean Santos <jeansantos.sw@gmail.com>
2025-09-10 16:34:30 +0100

    Chore: Config query client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { queryClient } from './api/react-query'
 import { routes } from './router/routes'
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster richColors />
       <RouterProvider router={routes} />
-    </>
+    </QueryClientProvider>
   )
 }
