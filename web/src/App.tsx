a077b3b8c10f546af96a6cddc74815f61eddd64f
Jean Santos <jeansantos.sw@gmail.com>
2025-09-10 12:58:42 +0100

    Chore: Setting prettier

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex h-screen items-center justify-center">
       <Button>Enviar</Button>
     </div>
   )
