c2e33f1a81f8984bb04ad019d5f8f9e3ea1c73d4
Jean Santos <jeansantos.sw@gmail.com>
2025-09-16 23:50:41 +0100

    Feat: Delete user

--- web/src/page/app/users/components/user-table.row.tsx
+++ web/src/page/app/users/components/user-table.row.tsx
@@ -1,5 +1,9 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { UserRoundPen, UserRoundX } from 'lucide-react'
+import { toast } from 'sonner'
 
+import { getProfile } from '@/api/services/profile-api/get-profile'
+import { deleteUsers } from '@/api/services/users-api/delete-users'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
@@ -9,6 +13,28 @@ import { UserDetails } from './user-datails'
 import { UserUpdate } from './user-update/user-update'
 
 export function UserTableRow({ user }: IUserTableRow) {
+  const queryClient = useQueryClient()
+
+  const { data: getProfileFn } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { mutate: deleteUserFn } = useMutation({
+    mutationFn: (userId: string) => deleteUsers(userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+      toast.success('Usuário excluído com sucesso!')
+    },
+    onError: () => {
+      toast.error('Falha ao excluir o usuário.')
+    },
+  })
+
+  function handleDeleteUser() {
+    deleteUserFn(user.id)
+  }
+
   return (
     <Dialog>
       <TableRow>
@@ -36,12 +62,14 @@ export function UserTableRow({ user }: IUserTableRow) {
           </DialogTrigger>
           <UserUpdate userId={user.id} />
         </TableCell>
-        <TableCell>
-          <Button variant="destructive" size="sm">
-            <UserRoundX className="h-3 w-3" />
-            Deletar
-          </Button>
-        </TableCell>
+        {getProfileFn?.role === 'admin' && (
+          <TableCell>
+            <Button onClick={handleDeleteUser} variant="destructive" size="sm">
+              <UserRoundX className="h-3 w-3" />
+              Deletar
+            </Button>
+          </TableCell>
+        )}
       </TableRow>
     </Dialog>
   )
